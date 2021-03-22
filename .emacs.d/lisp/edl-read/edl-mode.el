649e7cf2a54871ca487448ec9c373b66acb6fdff
charlie-gordon <prayadtangban@gmail.com>
2021-03-23 06:13:59 +0700

    Added syntax highlighting for packages

--- .emacs.d/lisp/edl-read/edl-mode.el
+++ .emacs.d/lisp/edl-read/edl-mode.el
@@ -1,3 +1,8 @@
+;; Syntax highlighting
+(setq edl-highlights
+      '(("span\\|xanalink" . font-lock-function-name-face)
+	("start\\|length" . font-lock-constant-face)))
+
 (define-derived-mode edl-mode fundamental-mode "EDL"
   "Major mode for editing EDL format"
-)  
+  (setq font-lock-defaults '(edl-highlights)))  
