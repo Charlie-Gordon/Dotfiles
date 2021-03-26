72d5f83eb46d0c586e5ded6b4b62c85096c62b16
termux <prayadtangban@gmail.com>
2021-03-26 08:54:57 +0700

    Removed X-related packages

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,3 @@
-;;; * Essential External Programs
-(load "~/.emacs.d/external/stuffs.el" t)
 ;;; * Emacs initialization
 ;;;; ** Use-package
 ;;;;; Initialize
@@ -27,16 +25,11 @@
 (require 'init-00-utils.el)
 ;;;; ** Interface tweaks
 (require 'init-10-face.el)
-;;;; ** LaTeX
-(require 'init-30-tex.el)
 ;;;; ** Org-mode
 (require 'init-31-org.el)
 ;;;; ** General Improvement
 ;;;;; Sentences end with a single space
 (setq sentence-end-double-space nil)
-;;;;; Allow access to content from clipboard
-(setq x-select-enable-clipboard t
-      x-select-enable-primary t)
 ;;;;; Customize file
 (setq custom-file (expand-file-name "lisp/custom.el" user-emacs-directory))
 ;;;;; Backup files
@@ -48,17 +41,15 @@
 (setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
 ;;;;; Lazy yes or no
 (fset 'yes-or-no-p 'y-or-n-p)
-;;;;; Keyboard layout
-(add-hook 'after-init-hook 'modremap)
 ;;; * Packages
 ;;;; Completions
 ;;;;; Selectrum
 (use-package selectrum
   :ensure t
-  :config
-  (selectrum-mode +1)
-  :bind (:map ctl-x-map
-	      ("C-f" . find-file)))
+  :config  (selectrum-mode +1))
+;;;;; Embark
+(use-package embark
+  :ensure t)
 ;;;;; Consult
 (use-package consult
   :bind (:map ctl-x-map
@@ -71,83 +62,6 @@
 (use-package marginalia
   :ensure t
   :config (marginalia-mode 1))
-;;;; EXWM
-(use-package exwm
-  :config
-  (use-package exwm-config
-    :ensure nil
-    :custom
-;;;;; Prefix keys to ignore
-    (exwm-input-prefix-keys . `(?\C-x
-				?\C-u
-				?\C-h
-			        ?\C-g
-			        ?\M-x
-				?\M-`
-				?\M-&
-				?\M-:))
-;;;;; Global keys for EXWM
-    (exwm-input-global-keys . `(([?\s-.] . reload-emacs-configuration)
-				([?\s-w] . exwm-workspace-switch)
-				([?\s-&] . (lambda (command)
-					     (interactive (list (read-shell-command "$ ")))
-					     (start-process-shell-command command nil command)))
-				([?\s-d] . modus-themes-toggle)
-				(,(kbd "s-<return>") . eshell)
-				,@(mapcar (lambda (i)
-					    `(,(kbd (format "s-%d" i)) .
-					      (lambda ()
-						(interactive)
-						(exwm-workspace-switch-create ,i))))
-					  (number-sequence 0 9))))
-;;;;; Line-editing keybindings for X windows
-    (exwm-input-simulation-keys .
-				'(([?\C-b] . [left])
-				  ([?\C-f] . [right])
-				  ([?\C-p] . [up])
-				  ([?\C-n] . [down])
-				  ([?\C-a] . [home])
-				  ([?\C-e] . [end])
-				  ([?\M-v] . [prior])
-				  ([?\C-v] . [next])
-				  ([?\C-d] . [delete])
-				  ([?\C-k] . [S-end C-x])
-				  ([?\C-y] . [C-v])))
-    :config
-    (exwm-config-example)
-;;;;; Set the initial workspace number
-    (unless (get 'exwm-workspace-number 'saved-value)
-      (setq exwm-workspace-number 4))
-;;;;; Make class name the buffer name
-    (add-hook 'exwm-update-class-hook
-	      (lambda ()
-      	(exwm-workspace-rename-buffer exwm-class-name)))
-;;;;; Firefox keybindings
-    (add-hook 'exwm-manage-finish-hook  (lambda ()
-					  (when (and exwm-class-name
-						     (string= exwm-class-name "Firefox"))
-					    (exwm-input-set-local-simulation-keys
-					     `( ,@exwm-input-simulation-keys
-						([?\C-\M-j] . ,(kbd "C-<tab>"))
-						([?\C-\M-k] . ,(kbd "C-S-<tab>"))
-						([?\C-\M-h] . ,(kbd "M-<left>"))
-						([?\C-\M-l] . ,(kbd "M-<right>"))
-						([?\C-l] . [f6])
-						([?\C-q] . ?\C-w)
-						([?\C-w] . ?\C-c)
-						([?\C-y] . ?\C-v)
-						([?\C-s] . ?\C-f)
-      						([?\C-\S-s] . ?\C-g)
-						([?\C-m] . ?\')
-						([?\M-a] . [C-home])
-						([?\M-e] . [C-end])
-						([?\M-f] . ,(kbd "C-<left>"))
-       						([?\M-b] . ,(kbd "C-<right>"))
-						(,(kbd "C-/") . [?\C-z]))))))    
-;;;;; Prefered minibuffer    
-    (selectrum-mode +1)
-;;;;; Enable EXWM    
-    (exwm-enable)))
 ;;;; Yasnippet
 (use-package yasnippet
   :config
@@ -155,47 +69,6 @@
   (yas-load-directory (concat user-emacs-directory "snippets"))
   (yas-reload-all)
   (yas-global-mode 1))
-;;;; Nov.el
-(use-package nov
-  :config
-  (add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
-  :bind (:map nov-mode-map
-	      ("C-S-n" . shr-next-link)
-	      ("C-S-p" . shr-previous-link)))
-;;;; pdf-tools & restore functions
-(use-package pdf-tools
-  :init
-  (pdf-tools-install)
-  :config
-  (setq-default pdf-view-display-size 'fit-page)
-  :bind (:map pdf-view-mode-map
-	      ("g"  . pdf-view-first-page)
-	      ("G"  . pdf-view-last-page)
-	      ("l"  . image-forward-hscroll)
-	      ("h"  . image-backward-hscroll)
-	      ("j"  . pdf-view-next-page)
-	      ("k"  . pdf-view-previous-page)
-	      ("e"  . pdf-view-goto-page)
-	      ("y"  . pdf-view-kill-ring-save)
-	      ("i"  . pdf-misc-display-metadata)
-       	      ("o"  . pdf-occur)
-	      ("b"  . pdf-view-set-slice-from-bounding-box)
-	      ("r"  . pdf-view-reset-slice)
-	      ("R"  . pdf-view-revert-buffer)
-	      ("C-s" . isearch-forward)
-	      ("C-c C-c" . image-toggle-display))
-  :custom
-  (yas-minor-mode nil)
-  (pdf-cache-image-limit 32)
-  (pdf-view-max-image-width 2048)
-  (pdf-view-resize-factor 1.8)
-  (pdf-isearch-batch-mode t)
-  (pdf-annot-activate-created-annotations t))
-(use-package pdf-view-restore
-  :after pdf-tools
-  :config
-  (add-hook 'pdf-view-mode-hook 'pdf-view-restore-mode)
-  (setq pdf-view-restore-filename "~/.emacs.d/.pdf-view-restore"))
 ;;;; with-editor
 (use-package with-editor)
 ;;;; Magit
