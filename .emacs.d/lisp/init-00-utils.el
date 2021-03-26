27fb7b29eee3a7387b84849a5f7e1f093e0d3e87
charlie-gordon <prayadtangban@gmail.com>
2021-03-26 17:29:06 +0700

    Removed excess functions.

--- .emacs.d/lisp/init-00-utils.el
+++ .emacs.d/lisp/init-00-utils.el
@@ -8,23 +8,15 @@
   "Dired with root permission"
   (interactive "D")
   (dired-at-point (concat "/doas:root@localhost:" (expand-file-name editing-directory))))
-;;;; Toggle theme function
- (defun modus-themes-toggle ()
-    "Toggle between `modus-operandi' and `modus-vivendi' themes."
-    (interactive) 
-  (if (eq (car custom-enabled-themes) 'modus-operandi)
-      (progn
-        (modus-themes-load-vivendi))
-    (modus-themes-load-operandi)))
-      
 ;;;; Find-file with root permissions
-(defadvice ido-find-file (after find-file-sudo activate)
+(defadvice find-file (after find-file-sudo activate)
   "Find file as root if necessary."
   (unless (and buffer-file-name
 	       (file-writable-p buffer-file-name))
     (find-alternate-file (concat "/doas:root@localhost:" buffer-file-name))))
 ;;;; EWW reflow document when scaled text
 (defun text-scale-mode-hook ()
+  "Rerender content of EWW when uses text-scale mode"
   (eww-reload :local))
 
 (provide 'init-00-utils.el)
