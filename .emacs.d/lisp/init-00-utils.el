ea78d84c359b7468c3ae8a0cb552e84083f69df5
charlie-gordon <prayadtangban@gmail.com>
2021-03-26 17:33:56 +0700

    Added shortcuts

--- .emacs.d/lisp/init-00-utils.el
+++ .emacs.d/lisp/init-00-utils.el
@@ -1,11 +1,12 @@
 ;;; init-00-utils.el --- Functions used by other el files
 ;;;; Reload configuration
 (defun reload-emacs-configuration ()
+  "Reload emacs' init.el file."
   (interactive)
   (load-file (concat user-emacs-directory "init.el")))
 ;;;; Dired with root permissions
 (defun sudired (editing-directory)
-  "Dired with root permission"
+  "Dired with root permission."
   (interactive "D")
   (dired-at-point (concat "/doas:root@localhost:" (expand-file-name editing-directory))))
 ;;;; Find-file with root permissions
@@ -16,9 +17,12 @@
     (find-alternate-file (concat "/doas:root@localhost:" buffer-file-name))))
 ;;;; EWW reflow document when scaled text
 (defun text-scale-mode-hook ()
-  "Rerender content of EWW when uses text-scale mode"
+  "Rerender content of EWW when uses text-scale mode."
   (eww-reload :local))
-
+;;;; Magit shortcuts
+(defun magit-status-dotfiles ()
+  "Open magit in dotfiles repository."
+  (magit-status "/yadm::"))
 (provide 'init-00-utils.el)
 
 
