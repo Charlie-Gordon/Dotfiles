e77a67c91e251efcfe66466d9985ad5ae7c65845
termux <prayadtangban@gmail.com>
2021-03-26 09:06:45 +0700

    Removed X-related section

--- .emacs.d/lisp/init-10-face.el
+++ .emacs.d/lisp/init-10-face.el
@@ -1,9 +1,4 @@
 ;;; init-10-face.el --- The look of emacs
-;;;; Screenful
-  (menu-bar-mode -1)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1)
-  (fringe-mode 1)
 ;;;; Remove lame startup screen
 (setq inhibit-startup-message t)
 ;;;; Time in modeline
@@ -22,9 +17,5 @@
 (prefer-coding-system 'utf-8)
 ;;;; Modus themes
 (use-package modus-themes)
-;;;; Iosevka font
-(set-face-attribute 'default nil :font "Iosevka SS09-14")
-;;;; Font for Thai character
-(set-fontset-font t 'thai (font-spec :family "TH SarabunPSK"
-				     :size 24))
+
 (provide 'init-10-face.el)
