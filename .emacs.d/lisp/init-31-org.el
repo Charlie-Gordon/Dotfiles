68d3a206f4dc3eb550e8a0067c15e5a7e2b8fbc8
termux <prayadtangban@gmail.com>
2021-03-26 09:07:52 +0700

    Removed disabled packages

--- .emacs.d/lisp/init-31-org.el
+++ .emacs.d/lisp/init-31-org.el
@@ -12,15 +12,6 @@
   :config
   (add-hook 'emacs-lisp-mode-hook 'outshine-mode))
 
-(use-package org-noter
-  :after org
-  :ensure t
-  :config
-  (setq org-noter-default-ntoes-file-naems '("notes.org")
-	org-noter-separate-notes-from-heading t)
-  (use-package org-noter-pdftools
-    :after org-noter))
-
 (use-package org-brain :ensure t
   :init
   (setq org-brain-path "~/journals/brain")
@@ -29,21 +20,10 @@
   (setq org-id-track-globally t)
   (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
   (add-hook 'before-save-hook #'org-brain-ensure-ids-in-buffer)
- ;; (push '("b" "Brain" plain (function org-brain-goto-end)
-   ;       "* %i%?" :empty-lines 1)
-    ;     org-capture-templates)
   (setq org-brain-visualize-default-choices 'all)
   (setq org-brain-title-max-length 12)
   (setq org-brain-include-file-entries nil
         org-brain-file-entries-use-title nil))
-
-(use-package polymode
-  :disabled
-  :config
-  (add-hook 'org-brain-visualize-mode-hook #'org-brain-polymode))
-
-(use-package org-pdftools
-  :hook (org-mode . org-pdftools-setup-link))
 
 (use-package anki-editor
   :after org-noter
