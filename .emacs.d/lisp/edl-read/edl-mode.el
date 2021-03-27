c212a56e58861ddf218fd7f68320eebf5ba79cd9
charlie-gordon <prayadtangban@gmail.com>
2021-03-27 09:34:07 +0700

    Added lexical binding

--- .emacs.d/lisp/edl-read/edl-mode.el
+++ .emacs.d/lisp/edl-read/edl-mode.el
@@ -1,4 +1,10 @@
-;; Syntax highlighting
+;;; edl-mode.el --- Major Mode for EDL files         -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  i-am
+
+;; Author: i-am <i@fbsd>
+;; Keywords: hypermedia
+
 (setq edl-highlights
       '(("span\\|xanalink" . font-lock-function-name-face)
 	("start\\|length" . font-lock-constant-face)))
