e44d0bdfd6aff4776f958bd6d6838f8ee09979d9
charlie-gordon <prayadtangban@gmail.com>
2021-05-15 18:45:22 +0700

    Screenshot function

--- .emacs.d/lisp/00-utils/init-00-utils.el
+++ .emacs.d/lisp/00-utils/init-00-utils.el
@@ -16,6 +16,22 @@
   (interactive)
   "Open magit in dotfiles repository."
   (magit-status "/yadm::"))
+;;;; Screenshot
+;; https://www.reddit.com/r/emacs/comments/idz35e/emacs_27_can_take_svg_screenshots_of_itself/
+(defun screenshot-svg ()
+  "Save a screenshot of the current frame as an SVG image.
+Saves to a temp file and puts the filename in the kill ring."
+  (interactive)
+  ;; Privacy
+  (make-variable-buffer-local 'display-time-mode)
+  (display-time-mode 0)
+  (force-mode-line-update)
+  (let* ((filename (make-temp-file "Emacs" nil ".svg"))
+         (data (x-export-frames nil 'svg)))
+    (with-temp-file filename
+      (insert data))
+    (kill-new filename)
+    (message filename)))
 ;;;; Occur
 (defun occur-list-urls (&optional to-buffer)
   "Produce buttonised list of all URLs in the current buffer."
