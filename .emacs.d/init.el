696a0c2d8d18263e406ae4e8698139d88a50d1c8
charlie-gordon <prayadtangban@gmail.com>
2021-06-10 18:46:13 +0700

    Factor out lisp/init-builtin.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -52,46 +52,18 @@
 (use-package init-outline :ensure nil)
 ;;;;;; EWW & SHR
 (use-package init-eww-shr :ensure nil)
+;;;;;; ERC
+(use-package init-erc :ensure nil)
+;;;;;; Repeat mode
+(use-package repeat
+  :ensure nil
+  :config
+  (repeat-mode 1))
+;;;;; Completions
+(use-package init-completion :ensure nil)
 ;;;;; Git
 (use-package init-git :ensure nil)
-;;;;; Completions & Navigation
-;;;;;; Completions
-;;;;;;;  Selectrum
-(use-package selectrum
-  :straight t
-  :custom
-  (selectrum-fix-vertical-window-height selectrum-max-window-height "Show as many candidates as possible")
-  :hook (after-init . selectrum-mode))
-;;;;;;;  Consult
-(use-package init-consult :ensure nil)
-;;;;;;;  Embark
-(use-package embark
-  :straight t
-  :bind
-  ("C-." . embark-act)
-  (:map embark-url-map
-	("s" . #'browse-url-xdg-open)
-	("m" . #'mpv-play-url))
-  (:map embark-symbol-map
-	("h" . #'helpful-at-point)))
-;;;;;;;  Orderless
-(use-package orderless
-  :straight t
-  :after selectrum
-  :custom
-  (selectrum-refine-candidates-function #'orderless-filter)
-  (selectrum-highlight-candidates-function #'orderless-highlight-matches)
-  (completion-styles '(orderless partial-completion)))
-;;;;;;;  Marginalia
-(use-package marginalia
-  :straight t
-  :after selectrum
-  :config (marginalia-mode 1))
-;;;;;; Navigation
-;;;;;;;  avy
-(use-package avy
-  :straight t)
-;;;;; Application & utilities
+
 ;;;;;; Password-store
 (use-package pass
   :straight t)
@@ -100,95 +72,16 @@
   :straight t
   :custom
   (transmission-timer 30))
-;;;;; Multimedia
-;;;;;; EMMS
-(use-package emms
-  :straight t
-  :config
-  (use-package emms-setup
-    :ensure nil
-    :custom
-    (emms-source-file-default-directory "/storage/journals/resources/")
-    :config
-    (emms-all)
-    (emms-default-players))
-  (use-package emms-player-simple-mpv :disabled :straight t))
-;;;;;; MPV
-(use-package mpv
-  :straight t)
-;;;;;; LBRY
-
-(straight-use-package
- '(lbry-mode.el :type git :host gitlab :repo "Charlie-Gordon/lbry-mode-el"))
-
-;;;;;; parallel-mode.el
-
-(straight-use-package
- '(parallel-mode.el :type git :host gitlab :repo "Charlie-Gordon/parallel-mode.el"))
-
-;;;;;; Magit
-(use-package magit
-  :straight t
-  :requires with-editor tramp)
-;;;;; Note
-;;;;;; Nov.el
-(use-package nov
-  :straight t
-  :bind (:map nov-mode-map
-	      ("C-S-n" . shr-next-link)
-	      ("C-S-p" . shr-previous-link))
-  :mode (("\\.epub\\'" . nov-mode)))
-;;;;;; PDFs
-(use-package init-pdftools :ensure nil)
-;;;;; Language settings for prose and code
-;;;;;; Yasnippet
-(use-package yasnippet
-  :straight t
-  :config
-  (yas-load-directory (concat user-emacs-directory "snippets"))
-  (yas-global-mode))
-;;;;;; Markdown
-
-(use-package markdown-mode
-  :straight t
-  :disabled
-  :mode (("README\\.md\\'" . gfm-mode)
-         ("\\.md\\'" . markdown-mode)
-         ("\\.markdown\\'" . markdown-mode))
-  :commands (markdown-mode gfm-mode)
-  :init (setq markdown-command "multimarkdown"))
-
-;;;;;; Lisp
+;;;;; Lisp
 (use-package init-paredit :ensure nil)
-;;;;;;; Scheme
-
-(use-package quack
-  :straight t)
-
-(use-package geiser
-  :straight t
-  :config
-  (use-package geiser-mit :straight t))
-
-;;;;;;; Common-lisp
-
-(use-package cl-generic
-  :straight t)
-
-(use-package slime
-  :defer t
-  :custom
-  (inferior-lisp-program "/usr/local/bin/sbcl")
-  :config
-  (use-package slime-autoloads :ensure nil)
-  (slime-setup '(slime-fancy)))
-)
-
-(provide 'init)
+(use-package init-lisp :ensure nil)
+;;;;; Miscellaneous
+(use-package init-local :ensure nil)
+(use-package init-editing-utils :ensure nil)
+(use-package init-misc :ensure nil))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
-;;;;; Miscellaneous
-(use-package init-misc :ensure nil)
+(provide 'init)
 ;;; init.el ends here
