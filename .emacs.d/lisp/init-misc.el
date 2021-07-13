5891a956b5de622e7736ae57daf2ba1d5b5c3d75
Charlie-Gordon <prayadtangban@gmail.com>
2021-07-13 09:50:05 +0700

    More keybindings for helpful.

--- .emacs.d/lisp/init-misc.el
+++ .emacs.d/lisp/init-misc.el
@@ -7,13 +7,30 @@
 (use-package helpful
   :straight t
   :bind
+  ;; Note that the built-in `describe-function' includes both functions
+  ;; and macros. `helpful-function' is functions only, so we provide
+  ;; `helpful-callable' as a drop-in replacement.
+  ("C-h f" . helpful-callable)
   ("C-h v" . helpful-variable)
   ("C-h M" . helpful-macro)
   ("C-h o" . helpful-symbol)
   ("C-h c" . helpful-command)
   ("C-h C-k" . helpful-kill-buffers)
   ("C-h k" . helpful-key)
-  ("C-h f" . helpful-function))
+  ;; Lookup the current symbol at point. C-c C-d is a common keybinding
+  ;; for this in lisp modes.
+  ("C-c C-d" . helpful-at-point)
+  ;; Look up *F*unctions (excludes macros).
+  ;;
+  ;; By default, C-h F is bound to `Info-goto-emacs-command-node'. Helpful
+  ;; already links to the manual, if a function is referenced there.
+  ("C-h F" . helpful-function)
+  ;; Look up *C*ommands.
+  ;;
+  ;; By default, C-h C is bound to describe `describe-coding-system'. I
+  ;; don't find this very useful, but it's frequently useful to only
+  ;; look at interactive functions.
+  ("C-h C" . helpful-command))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
