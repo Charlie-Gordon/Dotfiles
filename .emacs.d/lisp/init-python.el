7aab0cb8c98fad1ef0b98e9dbe89ee5aa6a12786
Charlie-Gordon <prayadtangban@gmail.com>
2021-07-14 10:43:25 +0700

    Use elpy also.

--- .emacs.d/lisp/init-python.el
+++ .emacs.d/lisp/init-python.el
@@ -4,14 +4,19 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package pydoc-info
-  :straight t)
+  :straight t
+  :config
+  (info-lookup-add-help
+   :mode 'python-mode
+   :parse-rule 'pydoc-info-python-symbol-at-point
+   :doc-spec
+   '(("(python)Index" pydoc-info-lookup-transform-entry)
+     ("(sphinx)Index" pydoc-info-lookup-transform-entry))))
 
-(info-lookup-add-help
- :mode 'python-mode
- :parse-rule 'pydoc-info-python-symbol-at-point
- :doc-spec
- '(("(python)Index" pydoc-info-lookup-transform-entry)
-   ("(sphinx)Index" pydoc-info-lookup-transform-entry)))
+(use-package elpy
+  :straight t
+  :init
+  (elpy-enable))
 
 (provide 'init-python)
 ;;; init-python.el ends here
