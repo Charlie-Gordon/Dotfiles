b099592f6686f0e9df85941dbc9be1c586ffc36e
c1-g <char1iegordon@protonmail.com>
2021-08-27 16:50:34 +0700

    Remove. tao-theme

--- .emacs.d/lisp/init-face.el
+++ .emacs.d/lisp/init-face.el
@@ -4,9 +4,6 @@
   :config
   (load-theme 'modus-vivendi t)
   :straight t)
-;;;; Grayscale theme for the night
-(use-package tao-theme
-  :straight t)
 ;;;; Iosevka font
 (set-face-attribute 'default nil :font "Iosevka SS09-14")
 (set-face-attribute 'variable-pitch nil :font "Iosevka Aile Oblique")
