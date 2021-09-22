b7f2e901fa7a0f4e2beae0dcc5f7e2e80290f246
c1-g <char1iegordon@protonmail.com>
2021-09-22 11:13:52 +0000

    Load tex with straight.

--- .config/emacs/lisp/init-tex.el
+++ .config/emacs/lisp/init-tex.el
@@ -3,45 +3,43 @@
 ;;; Code:
 
 (use-package calctex
-  :disabled
   :straight '(calctex :type git :host github :repo "johnbcoughlin/calctex" :files ("calctex/calctex.el" "org-calctex/org-caltex.el")))
 
 (use-package tex
-  :straight auctex cdlatex
+  :straight auctex
+  :defer t
   :custom
-  (TeX-PDF-mode t
-   TeX-engine 'xetex
-   TeX-command-default "LaTeXMK"
-   latex-run-command "LaTeXMK"
-   LaTeX-command "LaTeXMK"
-   TeX-auto-save t
-   TeX-parse-self t
-   TeX-command-list
+  (TeX-PDF-mode t)
+  (TeX-engine 'xetex)
+  (TeX-command-default "LaTeXMK")
+  (latex-run-command "LaTeXMK")
+  (LaTeX-command "LaTeXMK")
+  (TeX-auto-save t)
+  (TeX-parse-self t)
+  (TeX-command-list
    '(("LaTeXMK" "latexmk -pvc -pdf %s" TeX-run-TeX nil t :help "Run latexmk")
-	 ("LuaLaTeX" "%`lualatex%(mode) --synctex=1 --8bit --shell-escape%' %t" TeX-run-TeX nil t :help "Run lualatex")
+     ("LuaLaTeX" "%`lualatex%(mode) --synctex=1 --8bit --shell-escape%' %t" TeX-run-TeX nil t :help "Run lualatex")
      ("XeLaTeX" "%`xelatex%(mode) -synctex=1 -8bit -shell-escape%' %t" TeX-run-TeX nil t :help "Run xelatex")
      ("Biber" "biber %s" TeX-run-Biber nil t :help "Run Biber")
      ("View" "%V" TeX-run-discard-or-function t t :help "Run Viewer")
      ("Index" "makeindex %s" TeX-run-command nil t :help "Create index file")
      ("Clean" "TeX-clean" TeX-run-function nil t :help "Delete generated intermediate files")
-     ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files")) 
-   LaTeX-verbatim-environments '("verbatim" "verbatim*" "Verbatim" "Verbatim*" "lstlisting" "code" "minted" "gascode" "ccode" "pythoncode" "javacode" "bashcode")   
-   TeX-view-program-selection '((output-pdf "PDF Tools") (output-html "xdg-open"))
-   TeX-view-program-list '(("pdf-tools" "TeX-pdf-tools-sync-view"))
-   TeX-view-style nil
-   bibtex-maintain-sorted-entries t
-   bibtex-align-at-equal-sign t)
+     ("Clean All" "(TeX-clean t)" TeX-run-function nil t :help "Delete generated intermediate and output files"))) 
+  (LaTeX-verbatim-environments '("verbatim" "verbatim*" "Verbatim" "Verbatim*" "lstlisting" "code" "minted" "gascode" "ccode" "pythoncode" "javacode" "bashcode"))   
+  (TeX-view-program-selection '((output-pdf "PDF Tools") (output-html "xdg-open")))
+  (TeX-view-program-list '(("pdf-tools" "TeX-pdf-tools-sync-view")))
+  (TeX-view-style nil)
+  (bibtex-maintain-sorted-entries t)
+  (bibtex-align-at-equal-sign t)
   :hook
   (LaTeX-mode . visual-line-mode) ;; Text wrap
   (LaTeX-mode . LaTeX-math-mode) ;; For formulas
   :config
   (add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
   (add-hook 'TeX-mode-hook #'(lambda () (setq TeX-command-default "LaTeXMK")))
-  (add-hook 'LaTeX-mode-hook #'(lambda () (setq TeX-command-default "LaTeXMK")))
-  (setq-default TeX-master nil))
+  (add-hook 'LaTeX-mode-hook #'(lambda () (setq TeX-command-default "LaTeXMK"))))
 
 (use-package reftex
-  :disabled
   :custom
   (reftex-plug-into-AUCTeX t
    reftex-bibliography-commands '("addbibresource" "bibliography" "nobibliography")
