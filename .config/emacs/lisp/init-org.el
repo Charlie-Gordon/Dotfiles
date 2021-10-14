64077519067cce1140aab1dec3f9b11059b43cbe
c1-g <char1iegordon@protonmail.com>
2021-10-14 11:12:53 +0700

    org: Add org-recur.

--- .config/emacs/lisp/init-org.el
+++ .config/emacs/lisp/init-org.el
@@ -126,6 +126,22 @@ Edna Syntax: org-anki-this!"
          "** REVIEW What of [[file:%F][%(file-name-sans-extension \"%f\")]]?")
         "\n")))))
 
+;;;; Recur
+
+(use-package org-recur
+  :hook ((org-mode . org-recur-mode)
+         (org-agenda-mode . org-recur-agenda-mode))
+  :straight t
+  :config
+  (define-key org-recur-mode-map (kbd "C-c d") 'org-recur-finish)
+
+  ;; Rebind the 'd' key in org-agenda (default: `org-agenda-day-view').
+  (define-key org-recur-agenda-mode-map (kbd "d") 'org-recur-finish)
+  (define-key org-recur-agenda-mode-map (kbd "C-c d") 'org-recur-finish)
+
+  (setq org-recur-finish-done t
+        org-recur-finish-archive t))
+
 ;;;; Outshine
 (use-package outshine
   :straight t
