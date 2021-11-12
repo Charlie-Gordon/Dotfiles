31fa63dccb773eb199515f3726e93ff7e03be67c
c1-g <char1iegordon@protonmail.com>
2021-11-12 22:48:07 +0700

    Config for org-agenda.

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -4,8 +4,6 @@
 ;;;; Add package sources
 (unless (assoc-default "melpa" package-archives) 
  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t))
-(unless (assoc-default "org" package-archives)
-  (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t))
 ;;;; Bootstrapping Use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -16,7 +14,29 @@
 (setq use-package-verbose t)
 ;;;; Org-mode
 (use-package org
-  :ensure nil)
+  :ensure nil
+  :config
+  (setq org-directory "~/storage/shared/Org/"))
+
+(use-package org-agenda
+  :ensure nil
+  :config
+  ;; Daniel Patru's answer at
+  ;; https://stackoverflow.com/questions/17215868/recursively-adding-org-files-in-a-top-level-directory-for-org-agenda-files-take
+  (defun org-get-agenda-files-recursively (dir)
+    "Get org agenda files from root DIR."
+    (directory-files-recursively dir "\.org$"))
+  (defun org-set-agenda-files-recursively (dir)
+    "Set org-agenda files from root DIR."
+    (setq org-agenda-files 
+	  (org-get-agenda-files-recursively dir)))
+  (defun org-add-agenda-files-recursively (dir)
+    "Add org-agenda files from root DIR."
+    (nconc org-agenda-files 
+	   (org-get-agenda-files-recursively dir)))
+  
+  (add-hook 'after-init-hook (lambda nil (org-set-agenda-files-recursively org-directory))))
+
 
 (use-package org-drill
   :ensure t)
