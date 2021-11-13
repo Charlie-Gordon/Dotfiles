c238428ae12eb3918ec354d68eea43a7a5cd03f3
c1-g <char1iegordon@protonmail.com>
2021-11-13 21:11:43 +0700

    Add local init file.

---
+++ .config/emacs/lisp/init-local.el
@@ -0,0 +1,26 @@
+;;; init-local.el --- Local configuration -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package password-store
+  :straight t)
+
+(use-package transmission
+  :straight t)
+
+(use-package trashed
+  :straight t)
+
+(use-package rainbow-mode
+  :straight t)
+
+(use-package vterm
+  :straight t)
+
+(use-package elfeed
+  :straight t)
+
+
+(provide 'init-local)
+;;; init-local.el ends here
