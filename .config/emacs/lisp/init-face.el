37c3b7c9020f2267c29363a27bb279684dc4140a
c1-g <char1iegordon@protonmail.com>
2022-01-13 06:53:35 +0700

    Remove rescaling Sans forgetica.

--- .config/emacs/lisp/init-face.el
+++ .config/emacs/lisp/init-face.el
@@ -9,7 +9,6 @@
 (set-face-attribute 'variable-pitch nil :font "Iosevka Aile Oblique")
 
 (add-to-list 'face-font-rescale-alist '("Umpush" . 0.85))
-(add-to-list 'face-font-rescale-alist '(".*sans forgetica.*" . 0.9))
 
 (provide 'init-face)
 ;;; init-face.el ends here
