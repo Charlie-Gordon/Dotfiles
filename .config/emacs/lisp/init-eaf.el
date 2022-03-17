09039a5fdaa5b66b6ea073fb7ba2af73beba8576
c1-g <char1iegordon@protonmail.com>
2022-03-17 15:06:35 +0700

    eaf: Remove eaf-interleave

--- .config/emacs/lisp/init-eaf.el
+++ .config/emacs/lisp/init-eaf.el
@@ -49,35 +49,5 @@
   (eaf-bind-key quit-window "q" eaf-pdf-viewer-keybinding)
   (eaf-bind-key jump_to_page "M-g l" eaf-pdf-viewer-keybinding))
 
-
-;;;###autoload
-(defun eaf-interleave--find-bibtex-note (filename)
-  (run-hook-with-args-until-success
-   'bibtex-completion-find-note-functions
-   (car (bibtex-completion-find-key-from-pdf filename))))
-
-(use-package eaf-interleave
-  :ensure nil
-  :disabled
-  :after (bibtex-completion eaf-pdf-viewer)
-  :hook
-  (org-mode . eaf-interleave-mode)
-  (eaf-pdf-viewer . eaf-interleave-app-mode)
-  :bind
-  (:map eaf-interleave-mode-map
-        ("M-." . eaf-interleave-sync-pdf-page-current)
-        ("M-n" . eaf-interleave-sync-pdf-page-next)
-        ("M-p" . eaf-interleave-sync-pdf-page-previous))
-  (:map eaf-interleave-app-mode-map
-        ("q" . eaf-interleave-quit)
-        ("M-i" . eaf-interleave-add-note)
-        ("M-." . eaf-interleave-sync-current-note)
-        ("M-N" . eaf-interleave-sync-next-note)
-        ("M-P" . eaf-interleave-sync-previous-note))
-  :config
-  (add-to-list 'eaf-interleave-org-notes-dir-list bibtex-completion-notes-path)
-  (setq eaf-interleave-disable-narrowing t
-        eaf-interleave--find-note-path-function #'eaf-interleave--find-bibtex-note))
-
 (provide 'init-eaf)
 ;;; init-eaf.el ends here
