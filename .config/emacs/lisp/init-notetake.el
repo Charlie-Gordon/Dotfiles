7f24cd2b2e8ee9e2ce88818bbc1bde03cf1b090f
c1-g <char1iegordon@protonmail.com>
2022-03-25 06:59:52 +0700

    org-fc: Less topic proportion

--- .config/emacs/lisp/init-notetake.el
+++ .config/emacs/lisp/init-notetake.el
@@ -487,7 +487,7 @@ Used to determines filename in `org-roam-capture-templates'."
   (org-fc-index-filter-function #'identity)
   (org-fc-algorithm 'roam-sm2)
   (org-fc-custom-contexts '((writing :paths "/storage/org/gtd/writing.org")))
-  (org-fc-topic-proportion 80)
+  (org-fc-topic-proportion 20)
   (org-fc-browser-headers
    '(("No." org-fc-browser-num>?)
      ("Title" nil)
