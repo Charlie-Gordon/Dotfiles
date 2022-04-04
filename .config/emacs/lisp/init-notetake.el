95ae82ce5b6ddd8fe0abac1a537a16718be6d152
c1-g <char1iegordon@protonmail.com>
2022-04-04 16:15:13 +0700

    org-noter-pdftools: Change highlight color to blue

--- .config/emacs/lisp/init-notetake.el
+++ .config/emacs/lisp/init-notetake.el
@@ -345,22 +345,23 @@ Used to determines filename in `org-roam-capture-templates'."
   :hook (org-mode . org-pdftools-setup-link))
 
 (use-package org-noter-pdftools
-    :after (:and org-noter org-pdftools)
-    :straight t
-    :config
-    ;; Add a function to ensure precise note is inserted
-    (defun org-noter-pdftools-insert-precise-note (&optional toggle-no-questions)
-      (interactive "P")
-      (org-noter--with-valid-session
-       (let ((org-noter-insert-note-no-questions (if toggle-no-questions
-                                                     (not org-noter-insert-note-no-questions)
-                                                   org-noter-insert-note-no-questions))
-             (org-pdftools-use-isearch-link t)
-             (org-pdftools-use-freepointer-annot t))
-         (org-noter-insert-note (org-noter--get-precise-info)))))
-
-    (with-eval-after-load 'pdf-annot
-      (add-hook 'pdf-annot-activate-handler-functions #'org-noter-pdftools-jump-to-note)))
+  :after (:and org-noter org-pdftools)
+  :straight t
+  :custom
+  (org-noter-pdftools-markup-pointer-color "#00bfff")
+  :config
+  ;; Add a function to ensure precise note is inserted
+  (defun org-noter-pdftools-insert-precise-note (&optional toggle-no-questions)
+    (interactive "P")
+    (org-noter--with-valid-session
+     (let ((org-noter-insert-note-no-questions (if toggle-no-questions
+                                                   (not org-noter-insert-note-no-questions)
+                                                 org-noter-insert-note-no-questions))
+           (org-pdftools-use-isearch-link t)
+           (org-pdftools-use-freepointer-annot t))
+       (org-noter-insert-note (org-noter--get-precise-info)))))
+  (with-eval-after-load 'pdf-annot
+    (add-hook 'pdf-annot-activate-handler-functions #'org-noter-pdftools-jump-to-note)))
 
 (use-package org-noter-media
   :straight '(org-noter-media :type git
