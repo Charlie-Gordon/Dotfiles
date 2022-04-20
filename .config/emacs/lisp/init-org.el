0b4bd801bd9875c5cabd96bdc215c44f08b47462
c1-g <char1iegordon@protonmail.com>
2022-04-20 10:33:53 +0700

    org: Add unixepoch to uuid format

--- .config/emacs/lisp/init-org.el
+++ .config/emacs/lisp/init-org.el
@@ -293,9 +293,9 @@ efforts may be updated by this function."
           (if my-created-property (progn
                                     ;; prefer date-stamp of CREATED property (if found):
                                     (setq my-created-datestamp (substring (org-entry-get nil "CREATED" nil) 1 11)) ;; returns "2021-12-16" or nil (if no CREATED property)
-                                    (setq new-id (concat my-created-datestamp "-" new-id)))
+                                    (setq new-id (concat my-created-datestamp "-" (round (time-to-seconds)) "-" new-id)))
             ;; use today's date-stamp if no CREATED property is found:
-            (setq new-id (concat (format-time-string "%Y-%m-%d-") new-id))))
+            (setq new-id (concat (format-time-string "%Y-%m-%d-%s-") new-id))))
         (org-set-property "ID" new-id))))
   (kill-new (concat "id:" (org-id-get))) ;; put ID in kill-ring
   (org-id-get) ;; retrieve the current ID in any case as return value
