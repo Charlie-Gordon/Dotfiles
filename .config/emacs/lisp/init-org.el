a660268a802e31843b7e567022d93a574feab26b
c1-g <char1iegordon@protonmail.com>
2022-04-20 10:21:12 +0700

    chore: Remove redundant codes

--- .config/emacs/lisp/init-org.el
+++ .config/emacs/lisp/init-org.el
@@ -124,7 +124,6 @@ it can be passed in POS."
   (org-resolve-clocks)
   :hook
   (org-mode . visual-line-mode)
-  (org-mode . org-modern-mode)
   (org-mode . mixed-pitch-mode)
   (org-mode . (lambda () (add-hook 'before-save-hook #'zp/org-set-last-modified nil t)))
   (org-clock-in . c1/org-set-todo-progress)
@@ -310,34 +309,6 @@ efforts may be updated by this function."
     (string-trim (buffer-substring (point) (progn (forward-sentence) (point))))))
 
 (advice-add 'org-id-get-create :override #'my-id-get-or-generate)
-
-(defun ap/org-tree-to-indirect-buffer (&optional arg)
-  "Create indirect buffer and narrow it to current subtree.
-The buffer is named after the subtree heading, with the filename
-appended.  If a buffer by that name already exists, it is
-selected instead of creating a new buffer."
-  (interactive "P")
-  (let* ((new-buffer-p)
-         (pos (point))
-         (buffer-name (let* ((heading (org-get-heading t t))
-                             (level (org-outline-level))
-                             (face (intern (concat "outline-" (number-to-string level))))
-                             (heading-string (propertize (org-link-display-format heading)
-                                                         'face face)))
-                        (concat heading-string "::" (buffer-name))))
-         (new-buffer (or (get-buffer buffer-name)
-                         (prog1 (condition-case nil
-                                    (make-indirect-buffer (current-buffer) buffer-name 'clone)
-                                  (error (make-indirect-buffer (current-buffer) buffer-name)))
-                           (setq new-buffer-p t)))))
-    (switch-to-buffer new-buffer)
-    (when new-buffer-p
-      ;; I don't understand why setting the point again is necessary, but it is.
-      (goto-char pos)
-      (rename-buffer buffer-name)
-      (org-narrow-to-subtree))))
-
-(advice-add 'org-tree-to-indirect-buffer :override 'ap/org-tree-to-indirect-buffer)
 
 (use-package oc
   :ensure nil
