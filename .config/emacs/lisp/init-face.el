d03f0a3004281c7617a55fb52efe218dd7f7d5bd
c1-g <char1iegordon@protonmail.com>
2022-05-01 10:48:23 +0700

    face: Add font for emoji

--- .config/emacs/lisp/init-face.el
+++ .config/emacs/lisp/init-face.el
@@ -13,6 +13,18 @@
 (set-face-attribute 'default nil :font "Iosevka SS09-14")
 (set-face-attribute 'variable-pitch nil :font "New Century Schoolbook" :height 175)
 
+(set-fontset-font
+ t
+ (if (version< emacs-version "28.1")
+     '(127744 . 129744)
+   'emoji)
+ (cond
+  ((member "Apple Color Emoji" (font-family-list)) "Apple Color Emoji")
+  ((member "Noto Color Emoji" (font-family-list)) "Noto Color Emoji")
+  ((member "Noto Emoji" (font-family-list)) "Noto Emoji")
+  ((member "Segoe UI Emoji" (font-family-list)) "Segoe UI Emoji")
+  ((member "Symbola" (font-family-list)) "Symbola")))
+
 (add-to-list 'face-font-rescale-alist '("Umpush" . 0.85))
 
 (provide 'init-face)
