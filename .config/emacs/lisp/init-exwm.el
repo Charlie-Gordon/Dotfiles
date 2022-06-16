3dc50a26f3f0f69886348aab3cf6347962eb1b4b
c1-g <char1iegordon@protonmail.com>
2022-06-16 15:40:00 +0700

    exwm: Add keybinding for elfeed

--- .config/emacs/lisp/init-exwm.el
+++ .config/emacs/lisp/init-exwm.el
@@ -48,7 +48,7 @@
     :config
     (setq
      ;; Prefix keys to ignore
-     exwm-input-prefix-keys '(?\C-x ?\C-u ?\C-h ?\C-g ?\C-' ?\C-, ?\M-x ?\M-` ?\M-& ?\M-: ?\s-o ?\s-c ?\s-v ?\s-j ?\s-m)
+     exwm-input-prefix-keys '(?\C-x ?\C-u ?\C-h ?\C-g ?\C-' ?\C-, ?\M-x ?\M-` ?\M-& ?\M-: ?\s-o ?\s-c ?\s-v ?\s-j ?\s-m ?\s-e)
      ;; Line-editing keybindings for X windows
      exwm-input-simulation-keys '(;; Backward-char
                                   ([?\C-b] . [left])
